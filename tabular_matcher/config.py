2fa9d45e77e592d1f84e021433fe2015a7e9f428
jtai-dev <jtai.dvlp@gmail.com>
2022-10-13 15:26:10 -0500

    first working prototype

---
+++ tabular_matcher/config.py
@@ -0,0 +1,150 @@
+from rapidfuzz import fuzz
+
+
+DEFAULT_THRESHOLD = 75.0
+DEFAULT_RAPIDFUZZ_SCORER = 'WRatio'
+SCORER_MODULE = 'fuzz'
+SCORERS_TO_EXCLUDE  = {'partial_ratio_alignment'}
+SCORERS = [attr for attr in dir(fuzz) 
+                if not attr.startswith('_')
+                and attr not in SCORERS_TO_EXCLUDE]
+
+
+class MatcherConfig:
+    
+    def __init__(self, x_columns:list, y_columns:list) -> None:
+
+        self.x_columns = x_columns or []
+        self.y_columns = y_columns or []
+
+        self.columns_to_match = _ColumnsToMatch(self)
+        self.threshold_by_column = _ThresholdByColumn(self)
+        self.scorer_by_column = _ScorerByColumn(self)
+
+        self.columns_to_get = _ColumnsToGet(self)
+        self.columns_to_group = _ColumnsToGroup(self)
+
+        self.total_required_threshold = DEFAULT_THRESHOLD
+
+    def clear_all(self):
+
+        self.columns_to_match.clear()
+        self.threshold_by_column.clear()
+        self.scorer_by_column.clear()
+        self.columns_to_get.clear()
+        self.columns_to_group.clear()
+
+    def populate_columns_to_match(self):
+        
+        self.columns_to_match.clear()
+
+        for column in sorted(set(self.x_columns).intersection(
+                             set(self.y_columns))):
+
+            self.columns_to_match.add(column, column)   
+
+
+class _ColumnsToMatch(dict):
+    def __init__(self, config:MatcherConfig):
+
+        self.__config = config
+
+    def add(self, x_column, *y_columns):
+
+        if x_column in self.__config.x_columns:
+
+            if x_column not in self.keys():
+                self[x_column] = []
+                self.__config.threshold_by_column.add(x_column)
+                self.__config.scorer_by_column.add(x_column)
+
+            if y_columns:
+                for y_column in set(y_columns).intersection(self.__config.y_columns):
+                    self[x_column].append(y_column)
+
+            return True
+
+        return False
+
+    def remove(self, x_column, *y_columns):
+
+        if x_column in self.keys():
+
+            for y_column in set(y_columns).intersection(self.__config.y_columns):
+                self[x_column].remove(y_column)
+
+            if not y_columns or not self[x_column]:
+                del self[x_column]
+                self.__config.threshold_by_column.remove(x_column)
+                self.__config.scorer_by_column.remove(x_column)
+        
+            return True
+
+        return False
+
+
+class _ThresholdByColumn(dict):
+
+    def __init__(self, config:MatcherConfig):
+        self.__config = config
+
+    def add(self, x_column, threshold=None):
+    
+        if x_column in self.__config.x_columns:
+            if threshold:
+                if 0 <= threshold <= 100:
+                    self[x_column] = threshold
+                    return True
+            else:
+                self[x_column] = DEFAULT_THRESHOLD
+                return True
+
+        return False
+
+    def remove(self, x_column):
+        if x_column in self.keys():
+            del self[x_column]
+
+
+class _ScorerByColumn(dict):
+
+    def __init__(self, config:MatcherConfig):
+        self.__config = config
+
+    def add(self, x_column, scorer=None):
+    
+        if x_column in self.__config.x_columns:
+            if scorer:
+                if scorer in SCORERS:
+                    self[x_column] = eval(f"{SCORER_MODULE}.{scorer}")
+                    return True
+            else:
+                self[x_column] = eval(f"{SCORER_MODULE}.{DEFAULT_RAPIDFUZZ_SCORER}")
+                return True
+
+        return False
+
+    def remove(self, x_column):
+        if x_column in self.keys():
+            del self[x_column]
+
+
+class _ColumnsToGet(list):
+    def __init__(self, config:MatcherConfig):
+        self.__config = config
+
+    def add(self, *y_columns):
+        for y_column in y_columns:
+            if y_column in self.__config.y_columns:
+                self.append(y_column)
+
+
+class _ColumnsToGroup(list):
+    def __init__(self, config:MatcherConfig):
+        self.__config = config
+
+    def add(self, *columns):
+        for column in columns:
+            if (column in self.__config.x_columns and
+                column in self.__config.y_columns):
+                    self.append(column)
---
+++ tabular_matcher/tabular_matcher.py
@@ -0,0 +1,153 @@
+from collections import defaultdict
+from .config import MatcherConfig
+
+
+def column_match(x_record:dict, 
+                 y_records:list,
+                 x_column:str,
+                 y_columns:list, 
+                 scorer,
+                 threshold=0):
+
+    scores = [max([scorer(x_record[x_column], 
+                          y_record[y_column], 
+                          score_cutoff=threshold) for y_column in y_columns
+                  ])
+                    for y_record in y_records]
+
+    return [(y_index, score) for y_index, score in enumerate(scores) if score] 
+
+
+def uniqueness(records, column):
+
+    items = {record[column] for record in records if record[column]}
+    return len(items)/len(records) if len(records) > 0 else 0
+
+
+def adjusted_uniqueness(records, columns):
+
+    u = {column: uniqueness(records, column) for column in columns}
+    return {column: value/sum(u.values()) for column, value in u.items() if sum(u.values())}
+
+
+def group_records_by_value(y_records, x_record, columns):
+
+    def _group():
+        column = columns.pop()
+        for y_record in y_records:
+            if y_record[column] == x_record[column]:
+                yield y_record
+
+    if not columns:
+        return y_records
+
+    else:
+        return group_records_by_value(list(_group()), x_record, columns)
+
+
+class TabularMatcher:
+
+    MATCH_STATUS = {0: 'unmatched',
+                    1: 'matched',
+                    2: 'ambiguous',
+                    3: 'review',
+                    4: 'duplicate'}
+
+    def __init__(self, x_records, y_records, config=None) -> None:
+        
+        self.config = MatcherConfig(None, None) if not config else config
+        self.x_records = x_records
+        self.y_records = y_records
+
+    @property
+    def x_records(self):
+        return self.__x_records.copy()
+
+    @x_records.setter
+    def x_records(self, records:list):
+        self.__x_records = records
+        columns = {header for record in records for header in record.keys()}
+
+        self.config.x_columns = list(columns)
+        self.config.clear_all()
+        self.config.populate_columns_to_match()
+
+    @property
+    def y_records(self):
+        return self.__y_records.copy()
+
+    @y_records.setter
+    def y_records(self, records:list):
+        self.__y_records = records
+        columns = {header for record in records for header in record.keys()}
+
+        self.config.y_columns = list(columns)
+        self.config.clear_all()
+        self.config.populate_columns_to_match()
+
+    def match(self):
+        
+        for x_index, x_record in enumerate(self.__x_records):
+
+            x_columns_to_match = [k for k, v in x_record.items() if k in self.config.columns_to_match.keys() 
+                                                                    and str(v).strip()]
+            u = adjusted_uniqueness(self.__y_records, x_columns_to_match)
+            grouped_y_records = group_records_by_value(self.__y_records, x_record, self.config.columns_to_group)
+
+            y_score_counter = defaultdict(float)
+
+            for x_column, y_columns in self.config.columns_to_match.items():
+
+                y_index_scores = column_match(x_record, 
+                                              grouped_y_records,
+                                              x_column, 
+                                              y_columns, 
+                                              scorer=self.config.scorer_by_column[x_column],
+                                              threshold=self.config.threshold_by_column[x_column])
+
+                for y_index, score in y_index_scores:
+                    y_score_counter[y_index] += u[x_column] * score if x_column in u.keys() else 0
+
+            y_matches = {k: v for k, v in y_score_counter.items() if v == max(y_score_counter.values()) 
+                                                                  and v >= self.config.total_required_threshold}
+
+            if not y_matches:
+                yield (x_index, y_matches, 0)
+
+            elif len(y_matches) > 1:
+                yield (x_index, y_matches, 2)
+
+            else:
+                optimal_threshold = sum([self.config.threshold_by_column[column] * u[column] for column in x_columns_to_match])
+                i = next(iter(y_matches))
+                    
+                if y_matches[i] <= optimal_threshold:
+                    yield (x_index, y_matches, 3)
+                else:
+                    yield (x_index, y_matches, 1)
+
+
+    def apply_matches(self, index_increment=0):
+
+        _x_records = self.x_records
+
+        for x_index, y_matches, status in self.match():
+
+            if y_matches and (status==1 or status==3):
+                y_index = next(iter(y_matches))
+
+                for column in self.config.columns_to_get:
+
+                    if column in self.config.x_columns:
+                        _x_records[x_index][f'_{column}_'] = self.__y_records[y_index][column]
+                    else:
+                        _x_records[x_index][column] = self.__y_records[y_index][column]
+
+            _x_records[x_index]['match_status'] = TabularMatcher.MATCH_STATUS[status].upper()
+            _x_records[x_index]['matched_with_row'] = ', '.join(map(lambda x: str(x + index_increment), 
+                                                                    y_matches.keys()))
+            _x_records[x_index]['match_score'] = ', '.join(map(lambda x: str(round(x, 2)), 
+                                                                y_matches.values()))
+
+        return _x_records
+    
